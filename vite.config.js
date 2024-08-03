d100f28837897572ec021956ae2c4de5cd63dd7a
youngkidwarrior <victor@she.energy>
2024-08-03 06:28:35 -0700

    Add vite-vercel-plugin

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import { rescriptRelayVitePlugin } from "rescript-relay-router";
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
   plugins: [
@@ -9,7 +10,18 @@ export default defineConfig({
     rescriptRelayVitePlugin({
       autoScaffoldRenderers: true,
     }),
+    vercel(),
   ],
+  vercel: {
+    rewrites: [{ "source": "/(.*)", "destination": "/client" }]
+  },
+  ssr: {
+    noExternal: [
+      // Work around the fact that rescript-relay is not yet an ESM module
+      // which messes up imports on NodeJs.
+      "rescript-relay",
+    ],
+  },
   server: {
     port: 9000,
   },
